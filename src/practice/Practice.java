74e27ac09eecaf77484b33d1abdee27788cec638
DevDev1010 <devde@sheridancollege.ca>
2024-06-17 11:52:06 -0400

    this is first version (original) of our main file

---
+++ src/practice/Practice.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practice;
+
+/**
+ *
+ * @author HP
+ */
+public class Practice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Some Sample text"); // this is the first change I made
+    }
+    
+}
